fb317e52e93fb61b89a6743ee73b5f0cab391594
akashgowda843129-sketch <akashgowda843129@gmail.com>
2026-03-31 13:31:34 +0530

    Add files via upload

---
+++ pr.py -3.py
@@ -0,0 +1,15 @@
+careect_pin="1234"
+balance=10000
+enter_pin=input("enter your pin")
+if("inter_pin==careect_ pin")
+print("pin verifind sacussfuly")
+amount= int(input("enter whit oarol amount"))
+if amount>= 0
+print("entred inualied amount")
+elsf amount> balance
+print("enter multiplies of 100")
+elif amat%100 !=0
+print("enter multiplies enter")
+else balance=balance-amount.
+balance=amount# balance= balance-amount
+print("saccus trasanstuom ")
